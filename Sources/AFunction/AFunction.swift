d13216aa93eea38ef3ed90b15be1f9fd31341cee
RapboyGao <atgao74201@qq.com>
2024-08-19 19:17:35 +0800

    2024.08.19 AFunction加入Description

--- Sources/AFunction/AFunction.swift
+++ Sources/AFunction/AFunction.swift
@@ -1,7 +1,7 @@
 import AValue
 import Foundation
 
-public struct AFunction: Sendable, Identifiable {
+public struct AFunction: Sendable, Identifiable, CustomStringConvertible {
     public let id: Int
     public let shortName: String
     public let arguments: Arguments
@@ -9,4 +9,18 @@ public struct AFunction: Sendable, Identifiable {
     public let part: Part
     public let examples: [ExampleArg]
     public let instance: @Sendable ([AValue]) throws -> AValue
+
+    public init(id: Int, shortName: String, arguments: Arguments, returnValue: Argument, part: Part, examples: [ExampleArg], instance: @escaping @Sendable ([AValue]) -> AValue) {
+        self.id = id
+        self.shortName = shortName
+        self.arguments = arguments
+        self.returnValue = returnValue
+        self.part = part
+        self.examples = examples
+        self.instance = instance
+    }
+
+    public var description: String {
+        "(" + arguments.description + ")->" + returnValue.name
+    }
 }
