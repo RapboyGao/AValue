6b636f1ad7fc699768382ecf119830297b0bfcdb
RapboyGao <atgao74201@qq.com>
2024-12-18 13:11:14 +0800

    2024.12.18 支持编辑HourMinute

---
+++ Sources/AValue/SpecialValues/HourMinute/AHMFormat.swift
@@ -0,0 +1,29 @@
+import Foundation
+
+public enum AHMParseError: Error {
+    case failedToParse
+}
+
+public struct AHMFormat: ParseableFormatStyle {
+    public var format: AHourMinuteValue.Format
+    public var parseStrategy: Strategy
+
+    public func format(_ value: Int) -> String {
+        AHourMinuteValue(minutes: value).toFormat(format).description
+    }
+
+    public struct Strategy: ParseStrategy {
+        public func parse(_ value: String) throws -> Int {
+            guard let parsed = [AHourMinuteValue](value)
+            else {
+                throw AHMParseError.failedToParse
+            }
+            return parsed.sum(format: .days24HM).toNumber()
+        }
+    }
+
+    public init(format: AHourMinuteValue.Format) {
+        self.format = format
+        self.parseStrategy = Strategy()
+    }
+}
---
+++ Sources/AValue/Views/TextFields/AHMTextfield.swift
@@ -0,0 +1,42 @@
+import SwiftUI
+
+#if os(iOS)
+
+@available(iOS 16, *)
+public struct AHMTextfield: View {
+    @Binding var value: Int?
+    @State var format: AHourMinuteValue.Format = .hourMinute
+    var allowSet: Bool
+    var placeholder: String
+
+    public var body: some View {
+        TextField(placeholder, value: $value, format: AHMFormat(format: format))
+            .aKeyboardView { uiTextfield in
+                AHMKeyboard(uiTextfield, format: $format)
+            }
+    }
+
+    public init(value: Binding<Int?>, allowSet: Bool, placeholder: String) {
+        self._value = value
+        self.allowSet = allowSet
+        self.placeholder = placeholder
+    }
+}
+
+@available(iOS 16, *)
+
+private struct Example: View {
+    @State var value: Int?
+    var body: some View {
+        AHMTextfield(value: $value, allowSet: true, placeholder: "Time")
+    }
+}
+
+@available(iOS 16, *)
+#Preview {
+    List {
+        Example()
+    }
+}
+
+#endif
