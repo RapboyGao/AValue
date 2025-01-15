ff71063fd4ed7cbc00d4c9b53c245fca680b8b6a
RapboyGao <atgao74201@qq.com>
2025-01-15 16:32:02 +0800

    2025.01.15 更新AValueArrayElement结构体，新增名称参数以支持更灵活的内容展示；重构初始化方法以包含名称和单位参数

--- Sources/AValue/AValueElement.swift
+++ Sources/AValue/AValueElement.swift
@@ -1,25 +1,29 @@
 import AUnit
 
 public struct AValueArrayElement: Hashable, Codable, Sendable, Identifiable {
+    /// 在数组中的索引
     public var id: Int
     public var value: AValue
     public var isLast: Bool
     public var isFirst: Bool
     public var unit: AUnit?
+    public var name: String?
 
-    public init(id: Int, value: AValue, isLast: Bool) {
+    public init(id: Int, value: AValue, isLast: Bool, name: String? = nil) {
         self.id = id
         self.value = value
         self.isLast = isLast
         self.isFirst = id == 0
+        self.unit = nil
     }
 
-    public init(id: Int, value: AValue, isLast: Bool, unit: AUnit) {
+    public init(id: Int, value: AValue, isLast: Bool, unit: AUnit, name: String? = nil) {
         self.id = id
         self.value = value
         self.isLast = isLast
         self.isFirst = id == 0
         self.unit = unit
+        self.name = name
     }
 }
 
